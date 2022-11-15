dc0bae67b3d4f5cbf90f778a5b23e9316274b29d
SiyuC <Siyu.Chen1902@xjtlu.edu.cn>
2022-11-15 22:49:06 +0800

    add UI description_page

---
+++ app/src/main/java/com/example/travelplan/description_page.java
@@ -0,0 +1,14 @@
+package com.example.travelplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class description_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description_page);
+    }
+}

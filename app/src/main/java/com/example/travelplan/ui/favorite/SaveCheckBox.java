5e91b9f85d350ea09ba2bae2d1a79625f704ef1f
xxx <80044699+luoluoluoxue@users.noreply.github.com>
2022-11-17 16:21:26 +0800

    add file about favorite

---
+++ app/src/main/java/com/example/travelplan/ui/favorite/SaveCheckBox.java
@@ -0,0 +1,21 @@
+package com.example.travelplan.ui.favorite;
+
+public class SaveCheckBox {
+
+
+        public String id;
+
+        public String title;
+
+        public String desc;
+
+        public boolean isCheck;
+
+        public SaveCheckBox(String id, String title, String desc) {
+            this.id = id;
+            this.title = title;
+            this.desc = desc;
+        }
+
+
+}

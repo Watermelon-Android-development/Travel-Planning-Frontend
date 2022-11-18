3b736daef2c69345d1131081dbcdf243b9fb872a
EddienBear <101927874+EddienBear@users.noreply.github.com>
2022-11-18 15:56:11 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/travelplan/ui/planlist/PlanlistAdapter.java
@@ -0,0 +1,114 @@
+package com.example.travelplan.ui.planlist;
+//
+//public class FavoriteAdapter {
+//}
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.travelplan.R;
+
+import java.util.List;
+
+
+public class PlanlistAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    private List<SaveCheckBox> mDatas;
+
+    private LayoutInflater mInflater;
+
+    public boolean flage_edit = false;
+    public boolean flage_visible = false;
+
+
+
+    public PlanlistAdapter(Context mContext, List<SaveCheckBox> mDatas) {
+        this.mContext = mContext;
+        this.mDatas = mDatas;
+
+        mInflater = LayoutInflater.from(this.mContext);
+
+    }
+
+    @Override
+    public int getCount() {
+        return mDatas.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mDatas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+
+        ViewHolder holder = null;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.activity_planlist_item, null);
+
+            holder = new ViewHolder();
+
+            holder.checkboxOperateData = (CheckBox) convertView.findViewById(R.id.checkbox_operate_data);
+            holder.textTitle = (TextView) convertView.findViewById(R.id.text_title);
+            holder.textDesc = (TextView) convertView.findViewById(R.id.text_desc);
+
+            convertView.setTag(holder);
+
+        } else {
+
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        final SaveCheckBox dataBean = mDatas.get(position);
+        if (dataBean != null) {
+            holder.textTitle.setText(dataBean.title);
+            holder.textDesc.setText(dataBean.desc);
+
+
+
+            if (flage_edit) {
+                holder.checkboxOperateData.setVisibility(View.VISIBLE);
+            } else {
+                holder.checkboxOperateData.setVisibility(View.GONE);
+            }
+
+            holder.checkboxOperateData.setChecked(dataBean.isCheck);
+
+
+            holder.checkboxOperateData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (dataBean.isCheck) {
+                        dataBean.isCheck = false;
+                    } else {
+                        dataBean.isCheck = true;
+                    }
+                }
+            });
+        }
+        return convertView;
+    }
+
+    class ViewHolder {
+
+        public CheckBox checkboxOperateData;
+
+        public TextView textTitle;
+
+        public TextView textDesc;
+    }
+}

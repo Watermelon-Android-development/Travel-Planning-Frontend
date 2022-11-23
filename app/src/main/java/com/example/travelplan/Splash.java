5d25729ece4049d2805a4f397da1f7e3c05ecc46
xxx <80044699+luoluoluoxue@users.noreply.github.com>
2022-11-23 10:17:42 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/travelplan/Splash.java
@@ -0,0 +1,25 @@
+package com.example.travelplan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class Splash extends Activity{
+    private final int SPLASH_DISPLAY_LENGHT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, HomePage.class);
+                Splash.this.startActivity(intent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}

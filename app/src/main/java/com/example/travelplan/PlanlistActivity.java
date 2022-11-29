e158e9893846e90ab3acf985eb2d66b7f7623d00
EddienBear <101927874+EddienBear@users.noreply.github.com>
2022-11-29 18:21:29 +0800

    Update PlanlistActivity.java

--- app/src/main/java/com/example/travelplan/PlanlistActivity.java
+++ app/src/main/java/com/example/travelplan/PlanlistActivity.java
@@ -1,17 +1,17 @@
 package com.example.travelplan;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import java.util.ArrayList;
 
 import android.os.Handler;
 
@@ -20,8 +20,8 @@ import java.util.List;
 import android.widget.ListView;
 
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.example.travelplan.ui.planlist.SaveCheckBox;
 import com.example.travelplan.ui.planlist.PlanlistAdapter;
 
 public class PlanlistActivity extends AppCompatActivity{
@@ -31,10 +31,67 @@ public class PlanlistActivity extends AppCompatActivity{
     private Button itemDelete;
     private ListView listView;
 
-    private List<SaveCheckBox> mDatas;
+    private List<TravelDatabaseHelper.Plan> mDatas;
 
+    private final TravelDatabaseHelper travelDatabaseHelper = new TravelDatabaseHelper(this);
     private PlanlistAdapter mAdapter;
     private Handler handler;
+
+    private class GetPlanTask extends AsyncTask<TravelDatabaseHelper.Plan, Void, Boolean> {
+        @Override
+        protected void onPreExecute(){
+        }
+
+        @Override
+        protected Boolean doInBackground(TravelDatabaseHelper.Plan... plans) {
+            try {
+                mDatas = travelDatabaseHelper.getAllPlans();
+                mAdapter = new PlanlistAdapter(PlanlistActivity.this, mDatas);
+            } catch (SQLiteException e){
+                return false;
+            }
+            return true;
+        }
+        @Override
+        protected void onPostExecute(Boolean success){
+            if(success){
+                listView = findViewById(R.id.planlistView);
+                listView.setAdapter(mAdapter);
+                Toast.makeText(PlanlistActivity.this, "All plans", Toast.LENGTH_SHORT);
+            }
+            else Toast.makeText(PlanlistActivity.this, "Database unavailable", Toast.LENGTH_SHORT);
+        }
+    }
+
+    private class DeletePlanTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected void onPreExecute(){
+        }
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try {
+                for (int i = 0,len=mDatas.size(); i < len; i++) {
+                    if (mDatas.get(i).isCheck){
+                        mDatas.remove(i);
+                        travelDatabaseHelper.deletePlans(mDatas.get(i).getTitle());
+                        len--;
+                        i--;
+                    }
+                }
+            } catch (SQLiteException e){
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success){
+            if (success) Toast.makeText(PlanlistActivity.this, "Delete successfully", Toast.LENGTH_SHORT).show();
+            else Toast.makeText(PlanlistActivity.this, "Database unavailable", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,24 +105,9 @@ public class PlanlistActivity extends AppCompatActivity{
 
         itemDelete.setVisibility(View.INVISIBLE);
         boxAllClick.setVisibility(View.INVISIBLE);
-        listView = (ListView) findViewById(R.id.planlistView);
-
-        mDatas = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-
-            SaveCheckBox dataBean = new SaveCheckBox("id" + i, "title"+ i, "description"+ i);
-            mDatas.add(dataBean);
-        }
 
 
-        mAdapter = new PlanlistAdapter(this, mDatas);
-        listView.setAdapter(mAdapter);
-
-        ActionBar actionBar=getSupportActionBar();
-        if(actionBar!=null){
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        new GetPlanTask().execute();
 
     }
 
@@ -100,12 +142,6 @@ public class PlanlistActivity extends AppCompatActivity{
             }
 
             mAdapter.notifyDataSetChanged();
-        }
-
-        switch (item.getItemId()){
-            case  android.R.id.home:
-                this.finish();
-                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -149,34 +185,18 @@ public class PlanlistActivity extends AppCompatActivity{
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
         builder.setTitle("DELETE");
         builder.setMessage("Are you sure to delete all the options?");
-        builder.setPositiveButton("CONFIRM", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                for (int i = 0,len=mDatas.size(); i < len; i++) {
-                    if (mDatas.get(i).isCheck==true){
-                        mDatas.remove(i);
-                        len--;
-                        i--;
-                    }
-                }
-                mAdapter.notifyDataSetChanged();
-            }
-
+        builder.setPositiveButton("CONFIRM", (dialog, which) -> {
+            new DeletePlanTask().execute();
+            mAdapter.notifyDataSetChanged();
         });
 
-        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+        builder.setNegativeButton("CANCEL", (dialog, which) -> {
 
-            }
         });
         AlertDialog dialog=builder.create();
         dialog.show();
     }
 
+
 }
-
-
-
 

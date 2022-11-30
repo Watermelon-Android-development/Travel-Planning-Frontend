120aa6ac6ccd237b234108acc028601944242c02
EddienBear <101927874+EddienBear@users.noreply.github.com>
2022-11-30 11:16:37 +0800

    Update PlandetailAdapter.java

--- app/src/main/java/com/example/travelplan/ui/plandetail/PlandetailAdapter.java
+++ app/src/main/java/com/example/travelplan/ui/plandetail/PlandetailAdapter.java
@@ -4,6 +4,8 @@ package com.example.travelplan.ui.plandetail;
 //}
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

2da717d4f2111ac799ba9607a4f19383c7caa11b
Mingzhi Gao <1372886433@qq.com>
2022-12-03 10:33:57 +0800

    修改最短路径算法

--- app/src/main/java/com/example/travelplan/ShortestRouteAlgorithm.java
+++ app/src/main/java/com/example/travelplan/ShortestRouteAlgorithm.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShortestRouteAlgorithm {
-    private static List<Integer> result;
+    private static List<Integer> origin, result;
     private static double shortestDistance = Double.POSITIVE_INFINITY;
     private static double[][] allLocations, allDistances;
     public static List<Integer> getShortestRoute(double[] userPosition, List<TravelDatabaseHelper.Site> sites) {
         allLocations = new double[sites.size() + 1][2];
         allDistances = new double[allLocations.length][allLocations.length];
         allLocations[0] = userPosition.clone();
+        origin = new ArrayList<>();
         for (int i = 0; i < sites.size(); i++) {
             allLocations[i+1] = new double[]{sites.get(i).getxCoor(), sites.get(i).getyCoor()};
+            origin.add(sites.get(i).getId());
         }
         for (int i = 0; i < allLocations.length; i++) {
             for (int j = 0; j < allLocations.length; j++) {
@@ -29,7 +31,7 @@ public class ShortestRouteAlgorithm {
 
     private static void dfs(boolean[] isVisited, int current, double distance, List<Integer> curResult) {
         isVisited[current] = true;
-        curResult.add(current);
+        curResult.add(origin.get(current));
         for (int i = 0; i < isVisited.length; i++) {
             if (!isVisited[i]) break;
             if (i == isVisited.length - 1) {
